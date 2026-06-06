dc86ff6022422c70a0a3324d6889171840e365fa
tanvimokal8578-cpu <tanvimokal8578@gmail.com>
2026-06-06 16:58:08 +0530

    Add files via upload

---
+++ include/footer.php
@@ -0,0 +1,33 @@
+      <footer class="footer">
+          <div class="container">
+
+
+              <div class="bottom-footer">
+                  <div class="row">
+                      <div class="col-xs-12 col-sm-3 payment-options color-gray">
+                          <h5>Payment Options</h5>
+                          <ul>
+                              <li>
+                                  <a href="#"> <img src="images/mastercard.png" alt="Mastercard"> </a>
+                              </li>
+                          </ul>
+                      </div>
+                      <div class="col-xs-12 col-sm-4 address color-gray">
+                          <h5> SoftWare Owner</h5>
+                          <p>Tanvi Mokal</p>
+                          <h5>Phone: +9270024940</a></h5>
+                      </div>
+                      <div class="col-xs-12 col-sm-5 additional-info color-gray">
+                          <h5>Addition informations</h5>
+                          <p>Enjoy The Testy And Fresh Food.</p>
+                      </div>
+                      <div class="col-xs-12 col-sm-4 address color-gray">
+                        <h5>Address</h5>
+                        <p>Shihu,Nagothane Pezari Road</p>
+                      </div>
+                  </div>
+              </div>
+
+          </div>
+      </footer>
+
